eb39f32d5cb637c88cf28057229bbace59a82b38
KPatel008 <122328545+KPatel008@users.noreply.github.com>
2024-01-27 10:44:21 -0500

    4 note Auto (Tested but still in prototype)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -140,7 +141,7 @@ public class RobotContainer {
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
         // return new exampleAuto(s_Swerve);
-        // return new PathPlannerAuto("Auto1");
-        return new PathPlannerAuto("Copy of Auto3");
+        //return new PathPlannerAuto("Auto1");
+        return new PathPlannerAuto("3 Note Auto");
     }
 }
