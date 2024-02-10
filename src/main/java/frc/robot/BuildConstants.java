9243f8415dbb3800f08c1a116584eceb4ee2d671
KPatel008 <122328545+KPatel008@users.noreply.github.com>
2024-02-09 19:37:25 -0500

    After drive P 2/8

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-Crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 66;
-  public static final String GIT_SHA = "7cc41fbab15bbf479b7c8b17c77552e658551a99";
-  public static final String GIT_DATE = "2024-02-06 21:54:35 EST";
+  public static final int GIT_REVISION = 67;
+  public static final String GIT_SHA = "d17bafa02ffec3d03658688e3c399f95508faf30";
+  public static final String GIT_DATE = "2024-02-07 12:09:11 EST";
   public static final String GIT_BRANCH = "Black_Rev2_Arm";
-  public static final String BUILD_DATE = "2024-02-07 12:03:02 EST";
-  public static final long BUILD_UNIX_TIME = 1707325382956L;
+  public static final String BUILD_DATE = "2024-02-09 17:02:37 EST";
+  public static final long BUILD_UNIX_TIME = 1707516157579L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
