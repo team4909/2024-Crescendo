9906943f273c484243a4b363f85a6beeb4deb848
taj-maharj08 <arjunmahajan@billericak12.com>
2024-02-16 16:03:24 -0500

    BB Example Path seems to be required

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-Crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 74;
-  public static final String GIT_SHA = "43954a1988846633155b9097cc0879bcb81c4a7c";
-  public static final String GIT_DATE = "2024-02-15 21:49:59 EST";
+  public static final int GIT_REVISION = 75;
+  public static final String GIT_SHA = "4369098ef301aa52b8fd41f6c67fac985980a371";
+  public static final String GIT_DATE = "2024-02-16 09:16:54 EST";
   public static final String GIT_BRANCH = "Black_Rev2_Arm";
-  public static final String BUILD_DATE = "2024-02-15 21:52:57 EST";
-  public static final long BUILD_UNIX_TIME = 1708051977662L;
+  public static final String BUILD_DATE = "2024-02-16 15:52:53 EST";
+  public static final long BUILD_UNIX_TIME = 1708116773973L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
